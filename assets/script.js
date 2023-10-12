f91ccf5d43683b800d0b54af56164aa9f941d3d7
Raszero <travisjcelli95@gmail.com>
2023-10-12 18:36:15 -0500

    created function within script.js to be called within start button function. restyle index.html so page can be modified dynamically

--- assets/script.js
+++ assets/script.js
@@ -1,57 +1,80 @@
 // d.query to pull from html
 let startEl = document.querySelector("#startButton");
-let winsEl = document.querySelector("#winCount");
-let lossesEl = document.querySelector("#lossCount");
+//let winsEl = document.querySelector("#winCount");
+//let lossesEl = document.querySelector("#lossCount");
 let timeLeftEl = document.querySelector("#timer");
-let resetTimeEl = document.querySelector("#resetButton");
-
+document.getElementById('clickArea')
 // variables to set score and time
 let timer = 5;
 let wins = 0;
 let losses = 0;
 let questionsCorrect = 0;
 let questionsToGuess = "";
-
+let currentQuestionIndex = 0;
 // questions to guess. Need to be objects instead of arrays.. An array of objects?
-let questions = {
-  question: "Commonly used data types DO NOT include?",
-  answer1: "Strings",
-  answer2: "Integers",
-  answer3: "Boolean",
-  Correct: "Variables",
-};
+let questions = [
+  {
+    question: "Commonly used data types DO NOT include?",
+    choices: ["Strings", "Integers", "Boolean", "Variables"],
+    answer: "Variables",
+  },
+  {
+    question: "Inside which HTML element do you put the Javascript?",
+    choices: ["<js>", "<scripter>", "<javascript>", "<script>"],
+    answer: "<script>",
+  },
+  {
+    question: "Where is the aplace to link the Javascript?",
+    choices: ["<head>", "<header>", "<footer>", "<body>"],
+    answer: "<body>",
+  },
+  {
+    question: "How do you write 'Hello World' in an alert box?",
+    choices: [
+      "msgBox('Hello World')",
+      "alertBox('Hello World')",
+      "msg('Hello World')",
+      "alert('Hello World')",
+    ],
+    answer: "alert('Hello World')",
+  },
+  {
+    question: "How do you call a function called 'myFunction'?",
+    choices: [
+      "call function myFunction()",
+      "function myFunction",
+      "You can not call functions",
+      "myFunction()",
+    ],
+    answer: "myFunction()",
+  },
+];
 
-questions = {
-  question: "Inside which HTML element do you put the Javascript?",
-  answer1: "<js>",
-  answer2: "<scripter>",
-  answer3: "<javascript>",
-  Correct: "<script>",
-};
+function nextQuestion() {
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionTitle = currentQuestion.question;
+  let questionAnswer = currentQuestion.answer;
+  let titleEl = document.createElement("h3");
+  let choices = currentQuestion.choices;
+  for (var i = 0; i < choices.length; i++) {
+    let choiceEl = document.createElement("button");
+    choiceEl.textContent = choices[i];
+  }
+}
 
-questions = {
-  question: "Where is the correct place to link the Javascript?",
-  answer1: "<head>",
-  answer2: "<header>",
-  answer3: "<footer>",
-  Correct: "<body>",
-};
+document.getElementById
+//linked to currentQuestion variable
+//.removeChild id=
+//creating question h3 .textContent
+//create choices as buttons
 
-questions = {
-  question: "How do you write 'Hello World' in an alert box?",
-  answer1: "msgBox('Hello World')",
-  answer2: "alertBox('Hello World')",
-  answer3: "msg('Hello World')",
-  Correct: "alert('Hello World')",
-};
-
-questions = {
-  question: "How do you call a function called 'myFunction'?",
-  answer1: "call function myFunction()",
-  answer2: "function myFunction",
-  answer3: "You can not call functions",
-  Correct: "myFunction()",
-};
+//for loop to call on question objects to run through questions (does not have to be randomized)
+//
+//use .removeElement(class, hide) so next div will be called on -clickevent-
+//
+//event listener to call on a question when start button is clicked
+//
+//something needs to be created dynamically?? choices of object array need to be created as buttons
 
 // create initial score count and storage for wins and losses functions
 function lose() {
@@ -65,8 +88,6 @@ function win() {
   document.location.reload();
 }
 
-// need init page load function
-
 // need function for start button. Upon click timer must start counting down and I want start button to disappear. Stop function??
 function startTimer() {
   var timerFunction = setInterval(function () {
@@ -76,28 +97,40 @@ function startTimer() {
       clearInterval(timerFunction);
       lose();
     }
-    // if (questionsCorrect ===) {
-    //   clearInterval(timerFunction);
-    //   win();
-    // }
+    if (questions === correct) {
+      clearInterval(timerFunction);
+      win();
+    }
+    console.log(timeLeftEl);
   }, 1000);
 }
 
+// question object needs to be randomized math.random math.floor
+
 function startGame() {
   startTimer();
+  nextQuestion();
   // Set all innerHTML of querySelectors above
   // if user guesses the question correctly, call win function
-  questionsToGuess = questions[Math.floor(Math.random() * questions.length)];
 
   for (var i = 0; i < questionsToGuess.length; i++) {}
 }
 
 startEl.addEventListener("click", function () {
   startGame();
+  //call multiple function within eventlistener
 });
 
-// question object needs to be randomized math.random math.floor
-
-// when question answered correctly win count must ++ and be presented with new randomized question (I want correct clicks to be green)
+// when question ed correctly win count must ++ and be presented with new randomized question (I want correct clicks to be green)
 
 // question answered wrong time is deducted from timer say 5 seconds (incorrect questions I want red) function to -- when wrong click area??
+
+// need init page load function
+function init() {
+  winsEl.innerHTML = JSON.parse(localStorage.getItem("wins")) || 0;
+  lossesEl.innerHTML = JSON.parse(localStorage.getItem("losses")) || 0;
+}
+
+init();
+
+// start function needs to hide screen and move to next
